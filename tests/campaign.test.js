6cada32d82707347602fe5d8817a8a9b6092a9ee
Yogesh Rao <yogesh.rao@contract.planetdds.com>
2022-12-13 10:31:32 +0530

    End to end functionality test case added

--- tests/campaign.test.js
+++ tests/campaign.test.js
@@ -62,4 +62,40 @@ describe('Campaign', () => {
         const request = await campaign.methods.requests(0).call();
         assert.equal('Buying car parts', request.description);
     });
+
+    it('process connot be complete since 50% percent of the contributors dont agree', async () => {
+        await campaign.methods.contribute().send({ from: accounts[7], value: '500'});
+        await campaign.methods.contribute().send({ from: accounts[8], value: '500'});
+        await campaign.methods.contribute().send({ from: accounts[9], value: '500'});
+
+        await campaign.methods.createRequest('Buying new bike', '1200', accounts[5]).send({ from: accounts[0], gas: 2000000 });
+
+        await campaign.methods.approveRequest(0).send({ from: accounts[8], gas: 2000000 });
+        try {
+            await campaign.methods.finalizeRequest(0).send({ from: accounts[0], gas: 20000000 });
+            assert(false);
+        } catch(error) {
+            assert(error);
+        }
+    });
+
+    it('complete campaign process', async () => {
+        await campaign.methods.contribute().send({ from: accounts[7], value: '500'});
+        await campaign.methods.contribute().send({ from: accounts[8], value: '500'});
+        await campaign.methods.contribute().send({ from: accounts[9], value: '500'});
+
+        await campaign.methods.createRequest('Buying new bike', '1200', accounts[5]).send({ from: accounts[0], gas: 2000000 });
+
+        await campaign.methods.approveRequest(0).send({ from: accounts[7], gas: 2000000 });
+        await campaign.methods.approveRequest(0).send({ from: accounts[8], gas: 2000000 });
+
+        await campaign.methods.finalizeRequest(0).send({ from: accounts[0], gas: 20000000 });
+
+        const request = await campaign.methods.requests(0).call();
+
+        let balance = await web3.eth.getBalance(accounts[5]);
+
+        assert(request.isComplete);
+        assert(balance > 100000000000000001000);
+    });
 });
