6da2aec07d4a70d327ea9f272218bdc0432f6069
Yogesh Rao <yogesh.rao@contract.planetdds.com>
2022-12-13 10:01:50 +0530

    Some test script added along with updated compiled script

--- tests/campaign.test.js
+++ tests/campaign.test.js
@@ -6,3 +6,53 @@ const web3 = new Web3(ganache.provider());
 
 const compiledCampaign = require('../ethereum/build/Campaign.json');
 const compiledCampaignFactory = require('../ethereum/build/CampaignFactory.json');
+
+let accounts;
+let campaign;
+let campaignfactory;
+let campaignAddress;
+
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+
+    campaignfactory = await new web3.eth.Contract(compiledCampaignFactory.abi)
+        .deploy({ data: compiledCampaignFactory.evm.bytecode.object })
+        .send({ from: accounts[0], gas: 2000000 });
+    
+    await campaignfactory.methods.createCampaign('100').send({ from: accounts[0], gas: 2000000 });
+
+    [campaignAddress] = await campaignfactory.methods.getCampaigns().call();
+
+    campaign = await new web3.eth.Contract(compiledCampaign.abi, campaignAddress);
+});
+
+describe('Campaign', () => {
+    it('deployes a campaign and the campaign factory', () => {
+        assert.ok(campaign.options.address);
+        assert.ok(campaignfactory.options.address);
+    });
+
+    it('the contract has been deployed by the manager', async () => {
+        const manager = await campaign.methods.manager().call();
+        assert.equal(manager, accounts[0]);
+    });
+
+    it('once contributed, contributor has been set properly', async () => {
+        await campaign.methods.contribute().send({
+            from: accounts[1],
+            value: '200'
+        });
+
+        const isContributor = await campaign.methods.contributors(accounts[1]).call();
+        assert(isContributor);
+    });
+
+    it('for contribution, a minimum amount of wei must be paid', async () => {
+        try {
+            await campaign.methods.contribute().send({ from: accounts[1], value: '50' });
+            assert(false);
+        } catch(error) {
+            assert(error);
+        }
+    });
+});
